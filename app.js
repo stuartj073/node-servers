ebef1e980edd32f488e0d9fd56b59d22489d20d3
stuartj073 <stuartj073@gmail.com>
2022-10-12 19:09:46 +0000

    Process.exit() purpose

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req);
+    // process.exit();  this will stop the node event loop;
 });
 
 server.listen(3000);
