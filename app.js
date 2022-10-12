4656eb93cccf2a6d1fa91b1ed81e075e57d5d240
stuartj073 <stuartj073@gmail.com>
2022-10-12 19:19:13 +0000

    Send response using 'res'

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><head><title>My first page</title><body><h1>Hello</h1></body></head></html>')
+    res.end();
+
     // process.exit();  this will stop the node event loop;
 });
 
